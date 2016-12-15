ccc86af821c9d7a57140fefc5d6e6f8e0fc9db2e
Schuyler Alschuler <Schuyler Alschuler>
2016-12-15 00:39:48 -0800

    Added animations

--- index.android.js
+++ index.android.js
@@ -1,4 +1,5 @@
-import { AppRegistry } from 'react-native';
+import { AppRegistry, UIManager } from 'react-native';
 import App from './src/App';
 
+UIManager.setLayoutAnimationEnabledExperimental(true); // need this line for animations!
 AppRegistry.registerComponent('tech_stack', () => App);
--- src/components/ListItem.js
+++ src/components/ListItem.js
@@ -1,22 +1,54 @@
 import React, { Component } from 'react';
-import { Text, TouchableWithoutFeedback, View, TouchableOpacity } from 'react-native';
+import { 
+  Text, 
+  TouchableWithoutFeedback, 
+  View, 
+  LayoutAnimation,
+  Platform,
+  UIManager
+} from 'react-native';
 import { connect } from 'react-redux';
 import { CardSection } from './common';
 import * as actions from '../actions';
 
 class ListItem extends Component {
 // props: this.props.library will be passed.
+  constructor(props) {
+    super(props);
+    //if (Platform.OS === 'android') {
+      //UIManager.setLayoutAnimationEnabledExperimental &&
+      //UIManager.setLayoutAnimationEnabledExperimental(true);
+    //}
+  }
+
+  componentWillUpdate() {
+    LayoutAnimation.linear();
+  }
+  
+  renderDescription() {
+    const { library, expanded } = this.props;
+    if (expanded) {
+      return (
+        <CardSection>
+          <Text style={{ flex: 1 }}>{library.description}</Text>
+        </CardSection>
+      );
+    }
+  }
+
   render() { 
     const { titleStyle } = styles;
     const { id, title } = this.props.library;
+
     return (
         <TouchableWithoutFeedback 
-          onPress={() => {console.log('pressed'); this.props.selectLibrary(id)}}
+          onPress={() => this.props.selectLibrary(id)}
         >
           <View>
             <CardSection>
               <Text style={titleStyle}>{title}</Text>
             </CardSection>
+            {this.renderDescription()}
           </View>
         </TouchableWithoutFeedback>
     );
@@ -30,6 +62,11 @@ const styles = {
   }
 };
 
-export default connect(null, actions)(ListItem); 
+const mapStateToProps = (state, ownProps) => { // you get access to its props in ownProps
+  const expanded = state.selectedLibraryId === ownProps.library.id;
+  return { expanded }; // this.props.expanded : expanded
+};
+
+export default connect(mapStateToProps, actions)(ListItem); 
 // null because I don't want access to the store (dont want to map to props)
 // passes the actions as props to this component
